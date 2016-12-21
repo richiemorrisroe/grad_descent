15535792b877fb07f9a35628e816742bf382584c
richie <richie.morrisroe@gmail.com>
2016-12-21 16:07:01 +0000

    some updates

--- polynomial/tests/testthat/test_first.R
+++ polynomial/tests/testthat/test_first.R
@@ -1,4 +1,4 @@
 
 require(testthat)
 test_that("20x can be converted to an expression",
-          expect_is(expression("20x"), "Expression"))
+          expect_is(to_expression("20x"), "Expression"))
